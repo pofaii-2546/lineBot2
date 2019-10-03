85ae096f054fb8f0df0ea6d69e765def66cb17e4
pofaii-2546 <56102916+pofaii-2546@users.noreply.github.com>
2019-10-03 15:53:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,13 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    sendText(user,userText)
+     if (userText == 'สวัสดี') :
+       sendText(user,'เออดีด้วย')
+    elif (userText  == 'ทำไรยุ') :
+       sendText(user,'ไม่บอก')
+    else :
+        pass
+
     return '',200
 
 def sendText(user, text):
